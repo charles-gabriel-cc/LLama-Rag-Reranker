6322e074a470f6e6f7206fd38932258ff107c444
cgcc <charles.gabriel.cc@gmail.com>
2025-02-11 02:39:40 -0300

    utils description

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,9 @@
 import inspect
 
 def get_valid_kwargs(cls, kwargs):
-    # Obtém a assinatura do método __init__ da classe
+    """
+    Extract args from a class
+    """
     sig = inspect.signature(cls.__init__)
-    # Pega os nomes dos parâmetros (removendo o 'self')
     valid_keys = set(sig.parameters.keys()) - {'self'}
-    # Retorna um dicionário filtrado apenas com os parâmetros válidos
     return {k: v for k, v in kwargs.items() if k in valid_keys}
